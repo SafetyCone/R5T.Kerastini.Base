3dd58c61b75398d3ac479aaa7fa260109567a461
MinexAutomation <minex.automation@gmail.com>
2020-05-09 17:07:57 -0700

    Added IVisualStudioProjectFileTemplate service definition interface.

---
+++ source/R5T.Kerastini.Base/Code/Services/Definitions/IVisualStudioProjectFileTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Gloucester.Types;
+
+
+namespace R5T.Kerastini
+{
+    public interface IVisualStudioProjectFileTemplate
+    {
+        ProjectFile Generate();
+    }
+}
